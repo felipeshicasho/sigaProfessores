201e8b65874958db32ce84e9d69ed27712010925
felipeshicasho <felipetoleds@gmail.com>
2024-11-07 09:48:34 -0300

    Preencher formulário corretamente.

--- js/login.js
+++ js/login.js
@@ -20,14 +20,33 @@ document.querySelector("#button__login").addEventListener("click", () => {
     ".input__login[type='password']"
   ).value;
 
-  if (usuarioInput && senhaInput != "" && document.querySelector("#button__professor").style.backgroundColor == "lightgreen") {
+  if (
+    usuarioInput &&
+    senhaInput != "" &&
+    document.querySelector("#button__professor").style.backgroundColor ==
+      "lightgreen"
+  ) {
     window.location.href = "professor.html";
-  }
-  else if( usuarioInput && senhaInput != "" && document.querySelector("#button__aluno").style.backgroundColor == "lightgreen")
-  {
+  } else if (
+    usuarioInput &&
+    senhaInput != "" &&
+    document.querySelector("#button__aluno").style.backgroundColor ==
+      "lightgreen"
+  ) {
     window.location.href = "aluno.html";
-  }
-  else {
-    alert("Por favor, preencha todos os campos.");
+  } else {
+    document.querySelector("#button__login").addEventListener("click", () => {
+      let alerta = document.querySelector("#alerta_texto");
+
+      // Verifica se o alerta já existe; se não, cria um novo
+      if (!alerta) {
+        alerta = document.createElement("p");
+        alerta.id = "alerta_texto";
+        alerta.innerText = "Preencha o formulário corretamente!";
+
+        const botaoLogin = document.querySelector("#button__login");
+        botaoLogin.parentNode.insertBefore(alerta, botaoLogin.nextSibling);
+      }
+    });
   }
 });
